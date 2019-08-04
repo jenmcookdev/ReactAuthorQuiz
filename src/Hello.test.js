8b72e9315152f26411e0995e11b7cdfa07dcc889
Jen Cook <kcjenmcook@gmail.com>
2019-08-04 01:26:26 -0500

    add gitignore

--- src/Hello.test.js
+++
@@ -1,7 +0,0 @@
-import { isTSAnyKeyword, exportAllDeclaration } from "@babel/types";
-
-describe("When setting up testing", () => {
-    isTSAnyKeyword("should fail", () => {
-        exportAllDeclaration(1 + 1).toBe(3);
-    });
-});
