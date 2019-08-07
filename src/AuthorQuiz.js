9e77574a40ad33f9301cbfaf17bce75e4c503764
Jen Cook <kcjenmcook@gmail.com>
2019-08-07 00:46:07 -0500

    added more to Author Quiz

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -11,8 +11,22 @@ function Hero() {
   </div>);
 }
 
-function Turn() {
-  return(<div/>);
+function Book({title}) {
+  return (<div className="answer">
+      <h4>{title}</h4>
+  </div>
+  );
+}
+
+function Turn({author, books}) {
+  return(<div className="row turn" style={{backgroundColor: "white"}}>
+    <div className="col-4 offset-1">
+      <img src={author.imageURL} className="authorimage" alt="Author"/>
+    </div>
+    <div className="col-6">
+      {books.map((title) => <Book title={title} key={title} />)}
+    </div>
+    </div>);
 }
 
 function Continue() {
@@ -28,17 +42,16 @@ function Footer() {
     </div>
     </div>);
 }
-class AuthorQuiz extends Component{
-render() {
+function AuthorQuiz({turnData}) {
   return (
       <div className="container-fluid">
         <Hero/>
-        <Turn/>
+        <Turn {...turnData} />
         <Continue />    
         <Footer/>
-          </div>
+      </div>
     );
   }
-}
+
 
 export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageURL: 'images/authors/marktwain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Huckleberry Finn',
+                'Life on the Mississippi',
+                'Roughing It'
+                ]
+    }
+];
+
+const state ={
+    turnData: {
+        author: authors[0],
+        books: authors[0].books
+    }
+};
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
